dba5d39956669a5a7a788cd8f62b1875a7daf72c
Nguyễn Đức Dũng <nguyenucung05082004@gmail.com>
2025-08-12 07:26:41 +0700

    gitignore

--- Debug/Core/Src/main.d
+++
@@ -1,3 +0,0 @@
-Core/Src/main.o: ../Core/Src/main.c \
- C:/Users/nguye/STM32CubeIDE/workspace_1.17.0/SMARTOFFICE/App/Inc/app_main.h
-C:/Users/nguye/STM32CubeIDE/workspace_1.17.0/SMARTOFFICE/App/Inc/app_main.h:
